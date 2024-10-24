4998f93b0adc494d95259912a90d6a880fefee19
Rafael Barrientos García <rafiprogramando@gmail.com>
2024-10-23 19:54:15 -0600

    add connection to database

---
+++ scripts/database.js
@@ -0,0 +1,36 @@
+const developers = [
+  {
+    name: "Rafael Barrientos",
+    role: "Rust Developer",
+  },
+  {
+    name: "Ana García",
+    role: "Frontend Developer",
+  },
+  {
+    name: "Laura Martínez",
+    role: "Backend Developer",
+  },
+  {
+    name: "Sofía Rodríguez",
+    role: "Full Stack Developer",
+  },
+  {
+    name: "María Fernández",
+    role: "DevOps Engineer",
+  },
+  {
+    name: "Gabriela López",
+    role: "Data Scientist",
+  },
+  {
+    name: "Patricia Jiménez",
+    role: "Mobile Developer",
+  },
+  {
+    name: "Isabel Sánchez",
+    role: "UI/UX Designer",
+  },
+];
+
+export default developers;
